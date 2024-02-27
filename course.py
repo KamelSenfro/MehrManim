8413adf1353b046f6a9ad5acb6639809c282efea
KamelSenfro <kamel.fares02@eng-st.cu.edu.eg>
2024-02-28 00:30:19 +0200

    s

--- course.py
+++ course.py
@@ -1,19 +1,12 @@
 #course
 from manim import *
 
-class Course(Scene):
+class Getters(Scene):
     def construct(self):
-        name = Text("Kamel", font="Arial", color=GOLD).to_edge(center_of_mass,buff=0.5) 
-        sq = Square(side_length=0.5, fill_color=GREEN).shift(LEFT * 3)
-        tri = Triangle().scale(0.5).to_edge(DR)
-        self.play(Write(name))
-        self.wait(1.5)
-        self.play(DrawBorderThenFill(sq))
-        self.wait(1.5)
-        self.play(DrawBorderThenFill(tri))
-        self.play(name.animate.to_edge(UR), run_time=2)
-        self.wait(1.5)
-        self.play(sq.animate.scale(2).shift(UP * 2), run_time=2)
-        
-
-#manim -pql course.py Course
+        rect = Rectangle(height=1, width=3, color=WHITE).to_edge(UL)
+        self.wait(1)
+        circ = Circle(radius=1, color=BLUE).to_edge(DOWN)
+        arrow = Arrow(start=rect.get_bottom(), end=circ.get_top(), color=WHITE) 
+        self.play(Create(VGroup(rect, circ, arrow)))
+        self.wait(1)
+        #manim -pql course.py Getters
